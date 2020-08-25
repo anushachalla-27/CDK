be42061f90f6ce5fc55f75213ec1e388e7709563
anushachalla-27 <69515847+anushachalla-27@users.noreply.github.com>
2020-08-25 17:42:22 +0530

    Update TestStudent.java

--- Core Java/Assignments/StudentAssignment/TestStudent.java
+++ Core Java/Assignments/StudentAssignment/TestStudent.java
@@ -16,20 +16,20 @@ public class TestStudent {
 
 	public static void main(String[] args) {
 		Student students[]=new Student[7];
-		students[0]=new EnggStudent("Srujan",1286,"IT","SVIT");
-		students[1]=new EnggStudent("Srikar",1285,"IT","SVIT");
-		students[2]=new EnggStudent("Vikram",1097,"cse","SVIT");
-		students[3]=new BscStudent("Srujan",3216,"science","MGR");
-		students[4]=new BscStudent("Srikar",3252,"Arts","MGR");
-		students[5]=new CommerceStudent("vikram",4123,"Science","KBR");
-		students[6]=new CommerceStudent("Srikar",4512,"Maths","KBR");
+		students[0]=new EnggStudent("Anusha",1286,"IT","SVIT");
+		students[1]=new EnggStudent("Divya",1285,"IT","SVIT");
+		students[2]=new EnggStudent("Kavya",1097,"cse","SVIT");
+		students[3]=new BscStudent("Navya",3216,"science","MGR");
+		students[4]=new BscStudent("Deepthi",3252,"Arts","MGR");
+		students[5]=new CommerceStudent("Sravya",4123,"Science","KBR");
+		students[6]=new CommerceStudent("Pravalika",4512,"Maths","KBR");
 		
-		CommerceStudent newstudent = new CommerceStudent("vikram",4123,"Science","KBR");
+		CommerceStudent newstudent = new CommerceStudent("Navya",4123,"Science","KBR");
 		if(search(students,newstudent))
 			System.out.println("Student found");
 		else
 			System.out.println("student not found");
-		EnggStudent newstudent1 = new EnggStudent("Srikar",1280,"IT","SVIT");
+		EnggStudent newstudent1 = new EnggStudent("Divya",1280,"IT","SVIT");
 		if(search(students,newstudent1))
 			System.out.println("Student found");
 		else
